e294a080a9cc2bbbd0126fc385492ffc386e1ec7
Ahmedelsayed265 <https://github.com/Ahmedelsayed265>
2024-03-20 20:58:28 +0200

    sound

--- src/components/layout/TestimonialCard.jsx
+++ src/components/layout/TestimonialCard.jsx
@@ -1,32 +1,55 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import StarsList from "./StarsList";
-import waves from "../../assets/images/waves.svg";
 import { BASE_URL } from "../../constants";
 
 const TestimonialCard = ({ feedback }) => {
   const [play, setPlay] = useState(false);
+  const [overlayWidth, setOverlayWidth] = useState(0);
   const audio = useRef();
+  const animationRef = useRef();
+
+  useEffect(() => {
+    return () => {
+      cancelAnimationFrame(animationRef.current);
+    };
+  }, []);
 
   const togglePlay = () => {
-    if (audio.current.paused) {
+    if (!play) {
       audio.current.play();
-      setPlay(true);
+      animateOverlay();
     } else {
       audio.current.pause();
       audio.current.currentTime = 0;
-      setPlay(false);
+      setOverlayWidth(0);
+      cancelAnimationFrame(animationRef.current);
+    }
+    setPlay(!play);
+  };
+
+  const animateOverlay = () => {
+    if (!audio.current.paused) {
+      const progress =
+        (audio.current.currentTime / audio.current.duration) * 100;
+      setOverlayWidth(progress);
+      animationRef.current = requestAnimationFrame(animateOverlay);
     }
   };
 
   const handleAudioEnd = () => {
     setPlay(false);
+    setOverlayWidth(0);
   };
 
   return (
     <div className="testimonial_card">
       <div className="sound">
-        <div className="img">
-          <img src={waves} alt="sound" />
+        <div className="waves_img">
+          <span className="wave"></span>
+          <span
+            className="overlay"
+            style={{ width: `${overlayWidth}%` }}
+          ></span>
         </div>
         <div className="play_btn" onClick={togglePlay}>
           {play ? (
