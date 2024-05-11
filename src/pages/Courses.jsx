a884ed29f2ef45648d191b4407c4c149715073c0
Ahmedelsayed265 <https://github.com/Ahmedelsayed265>
2024-05-11 13:55:34 +0300

    remove search

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 import CourseCard from "../components/layout/CourseCard";
-import { useTranslation } from "react-i18next";
+// import { useTranslation } from "react-i18next";
 import { useSelector } from "react-redux";
 
 const Courses = () => {
   const courses = useSelector((state) => state.courses.courses);
-  const { t } = useTranslation();
+  // const { t } = useTranslation();
   return (
     <section className="courses">
       <div className="container">
         <div className="row m-0">
-          <div className="col-12 p-2">
+          {/* <div className="col-12 p-2">
             <div className="search">
               <form action="" className="inner_search__form">
                 <input type="text" placeholder={t("lookingForSomthing")} />
               </form>
             </div>
-          </div>
+          </div> */}
           {courses?.map((course) => (
             <div className="col-lg-4 col-md-6 col-12 p-2" key={course.id}>
               <CourseCard course={course} />
