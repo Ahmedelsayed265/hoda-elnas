aa856595c3a52bb72370cd197f9db2d753e08b13
Ahmedelsayed265 <https://github.com/Ahmedelsayed265>
2024-05-17 16:27:36 +0300

    back link

--- src/pages/JoinSession.jsx
+++ src/pages/JoinSession.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { useTranslation } from 'react-i18next';
 
 const JoinSession = () => {
+  const { t } = useTranslation();
   const { vConnectLink } = useSelector((state) => state.vConnectLink);
   return (
     <section className="join_session">
+      <Link to="/my-courses">{t("backToCourses")}</Link>
       <iframe src={vConnectLink} title="session" frameborder="0"></iframe>
     </section>
   );
